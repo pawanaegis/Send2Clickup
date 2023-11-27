bbfc66d72e961b3d08ec48f906c59c85e79430ef
pawan <pawan@aegiscovenant.com>
2023-11-27 16:32:16 +0530

    trello popup close test 1

--- src/Components/Pages/Signup.jsx
+++ src/Components/Pages/Signup.jsx
@@ -7,63 +7,67 @@ const Signup =()=>{
     const [code, setCode] = useState(null);
     let [isLoading,setIsLoading] = useState(false);
     const navigate = useNavigate();
+
     useEffect(() => {
-        // Function to extract the code from the URL
         const extractCodeFromURL = () => {
-          const urlParams = new URLSearchParams(window.location.search);
-          const authorizationCode = urlParams.get('code');
-    
-          if (authorizationCode) {
+            const urlParams = new URLSearchParams(window.location.search);
+            const authorizationCode = urlParams.get('code');
 
-            setCode(authorizationCode);
-    
-            localStorage.setItem('code', authorizationCode);
+            if (authorizationCode) {
+                setCode(authorizationCode);
+                localStorage.setItem('code', authorizationCode);
+                setIsLoading(false);
 
-            setIsLoading(false);
+                // Close the new window
+                window.close();
 
-            window.close();
-            let t = window.TrelloPowerUp.iframe();
-            t.closePopup();
-            navigate('/send2clickup.html');
-            if (window.opener) {
-                window.opener.location.reload();
-              }
+                // Close the Trello popup (if using TrelloPowerUp)
+                let t = window.TrelloPowerUp.iframe();
+                t.closePopup();
+
+                // Communicate the code to the main Trello page
+                if (window.opener) {
+                    window.opener.postMessage({ code: authorizationCode }, '*');
+                }
+
+                // navigate('/send2clickup.html');
             }
         };
-    
+
         // Check if the authorization code is already in local storage
         const storedCode = localStorage.getItem('code');
         if (storedCode) {
             setCode(storedCode);
-            navigate('/send2clickup.html');
+            // navigate('/send2clickup.html');
         } else {
             extractCodeFromURL();
         }
 
+        // Listen for messages from the new window or iframe
+        window.addEventListener('message', handleMessage);
 
-        // Check if the new window is closed periodically
-        const checkWindowClosed = setInterval(() => {
-            if (!code && isLoading && window.closed) {
-                setIsLoading(false);
-                console.log(isLoading);
-            }
-        }, 500); // Adjust the interval as needed
-
-        // Clean up the interval when the component is unmounted
+        // Clean up event listeners when the component is unmounted
         return () => {
-            clearInterval(checkWindowClosed);
+            window.removeEventListener('message', handleMessage);
         };
-      }, [code,isLoading,navigate]);
+    }, [navigate]);
 
+    const handleMessage = (event) => {
+        // Handle messages from the new window or iframe
+        if (event.data && event.data.code) {
+            setCode(event.data.code);
+        }
+    };
 
     const handleLogin = () => {
         // Open the authorization URL in a new window
-        const newWindow = window.open(`${config.clickupURL}`, '_blank','width=640,height=480');
+        const newWindow = window.open(`${config.clickupURL}`, '_blank', 'width=640,height=480');
         setIsLoading(true);
+
         if (newWindow) {
-          newWindow.focus();
+            newWindow.focus();
         }
-      };
+    };
 
 
     return(
