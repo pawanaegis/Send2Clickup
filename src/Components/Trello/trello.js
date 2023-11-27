26f5f443e654d9e38998de491b50150003bfeb7c
pawan <pawan@aegiscovenant.com>
2023-11-27 23:17:52 +0530

    iframe test 7

--- src/Components/Trello/trello.js
+++ src/Components/Trello/trello.js
@@ -29,7 +29,7 @@ let TrelloPowerUp = () => {
 
  let TrelloPowerUpIframe = () => {
 var t = window.TrelloPowerUp.iframe();
-return t.set('member', 'private', 'code', 'kl3j4l32k4jl2k34jl3kj4l2').then((t)=>{
+return t.set('member', 'private', 'code', 'kl3j4l32k4jl2k34jl3kj4l2').then(()=>{
 t.getAll().then(function (data) {
     console.log(data);
   console.log(JSON.stringify(data, null, 2));
