e8af7435423bb9aa5b80363e6ead2594d31aa06b
pawan <pawan@aegiscovenant.com>
2023-11-29 21:53:23 +0530

    Userid upload test

--- src/Components/Pages/Signup.jsx
+++ src/Components/Pages/Signup.jsx
@@ -38,7 +38,7 @@ const Signup =()=>{
           
           if (authorizationCode) {
             setCode(authorizationCode);
-            let data2 = {ClickupCode:authorizationCode}
+            let data2 = {ClickupCode:authorizationCode,Username:getTrelloCardData()}
             
             localStorage.setItem('code', authorizationCode);
             registerUser(data2).then(()=>{
@@ -75,9 +75,9 @@ const Signup =()=>{
     const handleLogin = () => {
         // Open the authorization URL in a new window
         const newWindow = window.open(`${config.clickupURL}`, '_blank','width=640,height=480');
-        let userData = {Username:getTrelloCardData()}
-        console.log(userData);
-        registerUser(userData);
+        //let userData = {Username:getTrelloCardData()}
+        // console.log(userData);
+        // registerUser(userData);
         setIsLoading(true);
         if (newWindow) {
           newWindow.focus();
