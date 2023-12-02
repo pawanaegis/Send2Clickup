4a5acbe35f4d8be14ca9acaf43649d40b2df78c6
pawan <pawan@aegiscovenant.com>
2023-12-03 02:32:20 +0530

    Auth saved in new table test 1

--- src/Components/Pages/Signup.jsx
+++ src/Components/Pages/Signup.jsx
@@ -12,9 +12,10 @@ const Signup =()=>{
     const registerUser = async(data) =>{
       try {
           let memberData = await getMemberData();
+          console.log(memberData.fields);
           const response = await axios.post(
             `https://api.airtable.com/v0/${config.airtable_base}/${config.airtable_table_2}`,
-            { fields: data, ...memberData },
+            { fields: data, ...memberData.fields },
             {
               headers: {
                 Authorization: `Bearer ${config.airtable_api}`,
