c0764fbea56a52f7e0901ba0f22e5cd21536d568
pawan <pawan@aegiscovenant.com>
2023-12-02 22:53:27 +0530

    fixing null value of card desc test 2

--- src/Components/Trello/trello.js
+++ src/Components/Trello/trello.js
@@ -95,17 +95,17 @@ var btnCallback = function (t, opts) {
 
                  var data = {
                   fields:{
-                   trelloCardId: context.card || '',
-                   trelloMemberId: context.member || '',
-                   trelloBoardId: context.board || '' ,
-                   trelloUsername: memberData.username || '',
-                   clickupCode: code || '',
-                   clickupToken: token || '',
-                   cardDescription: cardData.desc || '',
-                   cardName: cardData.name || '',
-                   membersAssigned: cardData.members || '',
-                   cardStartDate: cardData.start || '',
-                   cardDueDate: cardData.due || '',
+                   trelloCardId: context.card || "",
+                   trelloMemberId: context.member || "",
+                   trelloBoardId: context.board || "" ,
+                   trelloUsername: memberData.username || "",
+                   clickupCode: code || "",
+                   clickupToken: token || "",
+                   cardDescription: cardData.desc || "",
+                   cardName: cardData.name || "",
+                   membersAssigned: cardData.members || "",
+                   cardStartDate: cardData.start || "",
+                   cardDueDate: cardData.due || "",
                   }
         
                  }    
@@ -117,7 +117,7 @@ var btnCallback = function (t, opts) {
                  let requestOptions = {
                   method: 'POST',
                   headers: myHeaders,
-                  body: data,
+                  body: JSON.stringify(data),
                   redirect: 'follow'
                 };
                 console.log(requestOptions.headers);
