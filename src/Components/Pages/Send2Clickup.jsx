de13a7c039b5fc8bd0dd7c2809d3573286af403c
pawan <pawan@aegiscovenant.com>
2023-12-03 00:02:07 +0530

    test data upload to airtable test 7

--- src/Components/Pages/Send2Clickup.jsx
+++ src/Components/Pages/Send2Clickup.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import axios from 'axios';
+import config from '../config';
+
 
 export default function Send2Clickup() {
-  
+  let [isLoading, setIsLoading] = useState(false);
+
+  let sendCardToClickup= async(data) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        `https://api.airtable.com/v0/${config.airtable_base}/${config.airtable_table}`,
+        { fields: data },
+        {
+          headers: {
+            Authorization: `Bearer ${config.airtable_api}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      setIsLoading(false);
+      console.log('Record created successfully:', response.data);
+  } catch (error) {
+    console.error('Error creating record:', error);
+    // Handle error, show error message, etc.
+  }
+     
+  }
   return (
+   
     <>
     <h3>Send2Clickup Card Preview</h3>
      <div>
+        <h1>Send Card to Clickup</h1>
+        <Button
+      variant="contained"
+      color={'primary'}
+      disableElevation
+      onClick={sendCardToClickup}
+      disabled={isLoading}
+      startIcon={isLoading && <CircularProgress size={20} color="inherit" />}
+      sx={{
+        '&.Mui-disabled': {
+            backgroundColor: '#FF00FF', // Red color for disabled state
+            color: '#FFFFFF', // Text color for disabled state
+            fontWeight: 'bold',
+        },
+        '&:hover': {
+            backgroundColor: '#FF00FF', // Red color for hover state
+        }, 
+        '&.MuiButton-root': {
+            fontWeight: 'bold',
+        }
+    }}
+    >
+      {isLoading ? 'Connecting...' : 'Connect ClickUp'}
+    </Button>
         <p style={{background:"green"}}>Clickup Connect.</p>
      </div>
     </>
--- src/Components/Trello/trello.js
+++ src/Components/Trello/trello.js
@@ -15,8 +15,6 @@ let TrelloPowerUp = () => {
           });
         },
           'card-buttons': function(t, options) {
-            var context = t.getContext();
-            console.log(JSON.stringify(context, null, 2));
             return [{
               icon: config.appLogo,
               text: 'Send2Clickup',
@@ -33,9 +31,14 @@ let TrelloPowerUp = () => {
           'card-detail-badges': function(t, options) {
             return [
               {
-                icon: config.appLogo,
-                text:'Add2Clickup',
-                callback: btnCallback
+                text:'Send',
+                callback: function(t) {
+                  return t.popup({
+                    title: 'Send Card to Clickup',
+                    url: t.signUrl('./send2clickup.html'),
+                    height: 300
+                  })
+                }
                 }]
               }
         })
@@ -55,76 +58,99 @@ let getTrelloCardData = () => {
     return "defaultData";
   }
 }
-
-var btnCallback = function (t, opts) {
-  return t.popup({
-    title: 'Send',
-    items: [{
-      text: 'Add to Clickup',
-      callback: async function (t) {
-
-        function closeBadge(t) {
-          return t.closePopup()
-        }
-
-        var memberData = await t.member("all");
-        console.log(memberData);
-        var code = await t.loadSecret('code')
-        .then(function (secret) {
-          console.log(typeof secret);
-          console.log("clickup code is",secret);
-          return code;
-        });
-        var token = await t.loadSecret('token').then(function(secret){
-          console.log("clickup token is",secret);
-          return secret;
-        })
-        var cardData = await t.card("all").then(function (card) {
-          console.log(JSON.stringify(card, null, 2));
-          return card;
-        });
-
-        var context = await t.getContext();
-
-                 var data = {
-                  fields:{
-                   trelloCardId: context.card || "",
-                   trelloMemberId: context.member || "",
-                   trelloBoardId: context.board || "" ,
-                   trelloUserId: memberData.username || "",
-                   trelloUsername: memberData.fullName || "",
-                   clickupCode: code || "",
-                   clickupToken: token || "",
-                   cardDescription: cardData.desc || "",
-                   cardName: cardData.name || "",
-                   membersAssigned: JSON.stringify(cardData.members) || "",
-                   cardDueDate: cardData.due || "",
-                  }
-        
-                 }    
-                 console.log(data);
-                 var myHeaders = new Headers();
-                  myHeaders.append("Content-Type", "application/json");
-                  myHeaders.append("Authorization", "Bearer pat8dFR91yJvAxN6v.b6879b08bd9556bcb2c7411e4a37208cc4336f8aef771db1e2a2b7ee3c1b0360");
-
-                 let requestOptions = {
-                  method: 'POST',
-                  headers: myHeaders,
-                  body: JSON.stringify(data),
-                  redirect: 'follow'
-                };
-                console.log(requestOptions.headers);
-   fetch("https://api.airtable.com/v0/appwtI4RvxKzIOeHB/Table 1", requestOptions)
-  .then(response => response.text())
-  .then(result =>{
-    console.log(result);
-    }).then(function() {
-      closeBadge();
-    })
-  .catch(error => console.log('error', error));
-      }
-    }]
+let sendCardToClickup = async(t) =>{
+  var memberData = await t.member("all");
+  console.log(memberData);
+  var code = await t.loadSecret('code')
+  .then(function (secret) {
+    console.log(typeof secret);
+    console.log("clickup code is",secret);
+    return code;
   });
-};
+  var token = await t.loadSecret('token').then(function(secret){
+    console.log("clickup token is",secret);
+    return secret;
+  })
+  var cardData = await t.card("all").then(function (card) {
+    console.log(JSON.stringify(card, null, 2));
+    return card;
+  });
 
-export {TrelloPowerUp, getTrelloCardData};
+  var context = await t.getContext();
+
+           var data = {
+            fields:{
+             trelloCardId: context.card || "",
+             trelloMemberId: context.member || "",
+             trelloBoardId: context.board || "" ,
+             trelloUserId: memberData.username || "",
+             trelloUsername: memberData.fullName || "",
+             clickupCode: code || "",
+             clickupToken: token || "",
+             cardDescription: cardData.desc || "",
+             cardName: cardData.name || "",
+             membersAssigned: JSON.stringify(cardData.members) || "",
+             cardDueDate: cardData.due || "",
+            }
+  
+           }    
+           console.log(data);
+           var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Authorization", "Bearer pat8dFR91yJvAxN6v.b6879b08bd9556bcb2c7411e4a37208cc4336f8aef771db1e2a2b7ee3c1b0360");
+
+           let requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: JSON.stringify(data),
+            redirect: 'follow'
+          };
+          console.log(requestOptions.headers);
+fetch("https://api.airtable.com/v0/appwtI4RvxKzIOeHB/Table 1", requestOptions)
+.then(response => response.text())
+.then(result =>{
+console.log(result);
+})
+.catch(error => console.log('error', error));
+}
+
+let data= async (t) => {
+  var memberData = await t.member("all");
+  console.log(memberData);
+  var code = await t.loadSecret('code')
+  .then(function (secret) {
+    console.log(typeof secret);
+    console.log("clickup code is",secret);
+    return code;
+  });
+  var token = await t.loadSecret('token').then(function(secret){
+    console.log("clickup token is",secret);
+    return secret;
+  })
+  var cardData = await t.card("all").then(function (card) {
+    console.log(JSON.stringify(card, null, 2));
+    return card;
+  });
+
+  var context = await t.getContext();
+
+           var data = {
+            fields:{
+             trelloCardId: context.card || "",
+             trelloMemberId: context.member || "",
+             trelloBoardId: context.board || "" ,
+             trelloUserId: memberData.username || "",
+             trelloUsername: memberData.fullName || "",
+             clickupCode: code || "",
+             clickupToken: token || "",
+             cardDescription: cardData.desc || "",
+             cardName: cardData.name || "",
+             membersAssigned: JSON.stringify(cardData.members) || "",
+             cardDueDate: cardData.due || "",
+            }
+  
+           }    
+           console.log(data);
+           return data;
+}
+export {TrelloPowerUp, getTrelloCardData, sendCardToClickup, data};
