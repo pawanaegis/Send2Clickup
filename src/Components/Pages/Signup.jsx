fa5a99868756e7d1e0f8c458ec4d2d6af6451d0f
pawan <pawan@aegiscovenant.com>
2023-12-03 20:59:38 +0530

    Token test on airtable and localstorage

--- src/Components/Pages/Signup.jsx
+++ src/Components/Pages/Signup.jsx
@@ -14,21 +14,14 @@ const Signup =()=>{
       try {
           let memberData = await getMemberData();
           console.log({clickupCode:code,...memberData.fields});
-          const token = await axios.post(`https://api.clickup.com/api/v2/oauth/token?client_id=${config.clickupClientId}&client_secret=${config.clickupSecret}&code=${code}`
-          ,{
-            headers:{
-                'Access-Control-Allow-Origin':' https://api.clickup.com',
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type',
-            }
-          }
-          )
-          console.log(token,"token");
+          const token = await axios.post(`https://api.clickup.com/api/v2/oauth/token?client_id=${config.clickupClientId}&client_secret=${config.clickupSecret}&code=${code}`)
+          console.log(token.data.access_token,"token");
+          localStorage.setItem('token', token.data.access_token);
           const response = await axios.post(
             `https://api.airtable.com/v0/${config.airtable_base}/${config.airtable_table_2}`,
             { fields: {
               clickupCode: code,
-              clickupToken: 'code',
+              clickupToken: token.data.access_token,
               ...memberData.fields
             } },
             {
